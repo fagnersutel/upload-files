243809e227ddb85e14aa811c4884c9852e9b98d3
Fagner Sutel <fagnersutel@gmail.com>
2017-08-06 11:19:30 -0300

    06-08-2017

---
+++ teste-form.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>PHP Pdf file Upload script : Example</title>
+</head>
+
+<body>
+
+<form method="post" action="upload.php" enctype="multipart/form-data"> 
+    <label>Arquivo:</label> 
+    <input type="file" name="arquivo" /> 
+    <input type="submit" value="Enviar" /> </form>
+
+
+
+</div>
+</body>
+</html>
